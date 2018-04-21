8476c9e828c7ae4155864c10d2a46f3d27f5494e
JC Mauras <jmauras24@yahoo.com>
2018-04-21 17:46:15 -0400

    fake data for products and prod relationship

--- server/db/index.js
+++ server/db/index.js
@@ -6,8 +6,19 @@ const Cart = require('./Cart');
 const Address = require('./Address');
 const CreditCard = require('./CreditCard');
 
+// Needed to fake data
+const faker = require('faker');
+const numCategories = 3;
+const numProducts = 10;
+// Model relationships
+
+Product.belongsTo(Category);
+Category.hasMany(Product);
+
+//TODO: Order, LineItems
+
 const sync = () => {
-  return conn.sync({ force: true })
+  return conn.sync({ force: true });
 };
 
 const seed = () => {
@@ -15,8 +26,25 @@ const seed = () => {
     Product.create({
       name: 'Mascara',
       description: 'GrandeLASH - MD Lash Enhancing Serum',
-      price: 65
+      price: 65.00
     })
+  .then(() => {
+      // for(var i = 0; i < numCategories; i++){
+      //   Category.create({
+      //     name: faker.commerce.department()
+      //   });
+      // };
+    })
+  .then(()=>{
+      for(var i = 0; i < numProducts; i++){
+        Product.create({
+          name: faker.commerce.productName(),
+          description: faker.lorem.sentence(),
+          price: faker.commerce.price()
+        });
+      };
+    })
+
   ])
 };
 
