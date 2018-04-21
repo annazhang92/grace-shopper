c78631ad384f0c03a9b661024d641e2ab7e4904f
JC Mauras <jmauras24@yahoo.com>
2018-04-21 18:20:31 -0400

    get all categories

---
+++ server/routes/api/categories.js
@@ -0,0 +1,10 @@
+const app = require('express').Router();
+const db = require('../../db');
+const { Category } = db.models;
+module.exports = app;
+
+app.get('/',(req, res, next) => {
+  Category.findAll()
+    .then( categories => res.send(categories))
+    .catch(next);
+});
