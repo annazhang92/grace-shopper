fabf1c9ef364178d6d276f752ae0c67983e8ce45
JC Mauras <jmauras24@yahoo.com>
2018-04-28 20:01:30 -0400

    props

--- client/components/RegisterForm.js
+++ client/components/RegisterForm.js
@@ -4,8 +4,8 @@ import { register } from '../store';
 
 
 class RegisterForm extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       firstName: '',
       lastName: '',
