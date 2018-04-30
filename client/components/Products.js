b0e64eae17d8f3d1be7243c50925580c6699057d
JC Mauras <jmauras24@yahoo.com>
2018-04-30 00:14:49 -0400

    specials ticker

--- client/components/Products.js
+++ client/components/Products.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 // this fixes image render from pinging the website too much at once
 import LazyLoad from 'react-lazy-load';
 import { PageHeader } from 'react-bootstrap';
@@ -28,8 +29,11 @@ class Products extends Component {
     const filteredProducts = products.filter( product => {
       return product.name.toLowerCase().indexOf(search.toLowerCase()) !== -1 && (parseFloat(product.price) > min) && ( max > 0 ? parseFloat(product.price) < max : true);
     });
+      const specials = products.filter( product => product.id > 6 && product.id < 12);
+
       return (
         <div>
+
         {
           (products && categories) ?
           <div>
@@ -40,7 +44,31 @@ class Products extends Component {
               <input type='numeric' step='0.01' name='min' value={min} placeholder="$ Min" onChange={this.updateSearch.bind(this)} />
               <span> - </span>
               <input type='numeric' step='0.01' name='max' placeholder="$ Max" onChange={this.updateSearch.bind(this)} />
-
+            </div>
+            <div>
+              <div className='ticker-wrap'>
+              <div className='ticker'>
+              {
+                specials.map(special =>{
+                  return(
+                    <div className='ticker__item card-ticker card-1'>
+                    <div style={{display:'flex', justifyContent:'row'}} >
+                    <span className="card-header">
+                      <img style={{height: '75px', width:'75px', padding:'4px'}} alt="product here" src={ special.imageUrl } />
+                    </span>
+                    <div>
+                    <Link to={ `/products/${special.id}` }>
+                      <span style={{fontSize: 16}}>{special.name}</span>
+                    </Link>
+                    </div>
+                    <span className='price' style={{fontSize: 16}}>: ${special.price}</span>
+                    </div>
+                    </div>
+                  )
+                })
+              }
+              </div>
+              </div>
             </div>
             <LazyLoad>
               <ProductCard products={ filteredProducts } />
