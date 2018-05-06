37dfef68ab95c990610395b19ee77d53d57de5fd
Anna Zhang <jz2345@nyu.edu>
2018-05-06 00:30:52 -0400

    non-login-user

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,28 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { PageHeader } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import ProductCard from './ProductCard';
 import store, { createOrder } from '../store';
 import { Link } from 'react-router-dom';
 
-const Cart = ({ products, thisUserProducts, thisUserlineItems, loggedIn, createOrder, user }) => {
-  const totalPrice = thisUserlineItems.reduce(function (acc, thisUserlineItem) {
-    return acc + Number(thisUserlineItem.price)* thisUserlineItem.quantity;
-  }, 0);
 
-  const orderDescription = thisUserlineItems.reduce(function (acc, thisUserlineItem) {
-    return acc + thisUserlineItem.name + thisUserlineItem.quantity +'/';
-  }, '');
+class Cart extends Component {
+  constructor({ products, thisUserProducts, thisUserlineItems, loggedIn, createOrder, user, lineItems }) {
+    super();
+    this.state = {
+      lineItems: []
+    };
+  }
 
-  return (
-    <div>
-      <PageHeader>Cart Items</PageHeader>
-      {thisUserlineItems && thisUserProducts ? <ProductCard products={ thisUserProducts } lineItems={thisUserlineItems}/> : <h2>Your cart is empty!</h2>}
-      <h2>TotalPrice: {totalPrice} dollar</h2>
-      <Link to={ `/orders/${user.id}` }><button onClick= { () => loggedIn? createOrder ({ description: orderDescription, price: totalPrice, userId: user.id, fullName: 'placeholder', address: 'placeholder', creditCardNumber: 12345678 }) : console.log ('please login') }>CheckOut</button></Link>
-    </div>
-  );
-};
+  componentWillMount() {
+    // localStorage.getItem('lineItems') && this.setState({lineItems: JSON.parse(localStorage.getItem('lineItems'))})
+    if(!this.props.loggedIn){
+    if(localStorage.getItem('lineItems')) {
+      const lineItems = JSON.parse(localStorage.getItem('lineItems'))
+      store.dispatch({
+        type: 'GET_LINEITEMS',
+        lineItems
+      })
+    }
+  }
+    
+  }
+
+  render() {
+    const { products, thisUserProducts, thisUserlineItems, loggedIn, createOrder, user, lineItems} = this.props;
+    const totalPrice = thisUserlineItems.reduce(function (acc, thisUserlineItem) {
+      return acc + Number(thisUserlineItem.price)* thisUserlineItem.quantity;
+    }, 0);
+
+    const orderDescription = thisUserlineItems.reduce(function (acc, thisUserlineItem) {
+      return acc + thisUserlineItem.name + thisUserlineItem.quantity +'/';
+    }, '');
+
+    // if (!loggedIn) {
+    //   const thisUserlineItems = lineItems;
+    //   const idArr = thisUserlineItems.map(thisUserlineItems => thisUserlineItems.productId);
+    //   const thisUserProducts = products.filter(product => idArr.includes(product.id));
+    //   console.log('wasds')
+    //   console.log(thisUserlineItems)
+    //   console.log(thisUserProducts)
+    // }
+
+    return (
+      <div>
+        <PageHeader>Cart Items</PageHeader>
+        {/* {thisUserlineItems && <p>{thisUserlineItems[0].name}</p>} */}
+        {thisUserlineItems && thisUserProducts ? <ProductCard products={ thisUserProducts } lineItems={thisUserlineItems}/> : <h2>Your cart is empty!</h2>}
+        <h2>TotalPrice: {totalPrice} dollar</h2>
+        <Link to={ `/orders/${user.id}` }><button onClick= { () => loggedIn? createOrder ({ description: orderDescription, price: totalPrice, userId: user.id, fullName: 'placeholder', address: 'placeholder', creditCardNumber: 12345678 }) : console.log ('please login') }>CheckOut</button></Link>
+      </div>
+    );
+  }
+}
 
 
 const mapStateToProps = ({ lineItems, user, products }) => {
@@ -32,9 +67,11 @@ const mapStateToProps = ({ lineItems, user, products }) => {
   const thisUserProducts = products.filter(product => idArr.includes(product.id));
   const loggedIn = !!user.id;
   return {
+    lineItems,
     thisUserlineItems,
     thisUserProducts,
     loggedIn,
+    products,
     user
   };
 }
--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -1,60 +1,134 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import store, { updateLineItem, deleteLineItem } from '../store';
 import { connect } from 'react-redux';
 
 import StarRatingComponent from 'react-star-rating-component';
 
-const ProductCard = ({ products, lineItems, updateLineItem, deleteLineItem }) => {
 
-  return (
-    <div className="wrapper">
-      {
-        products ?
-        products.map(product => {
-          const lineItem = lineItems ? lineItems.find(lineItem => lineItem.productId === product.id) : null;
-          return (
-            <div key={ product.id }>
-              <div className="card card-1">
-                <span className="card-header">
-                  <img alt="product here" src={ product.imageUrl } />
-                </span>
-                <Link to={ `/products/${product.id}` }>
-                  <h3>{product.name}</h3>
-                </Link>
-                <span className="price">
-                  Price: ${product.price}
-                </span>
-                <div style={{fontSize: 16, display:'flex',justifyContent:'center'}}>
-                  <StarRatingComponent
-                    name="rate1"
-                    editing={false}
-                    starCount={5}
-                    value={4.5}
-                  />
-                  <span>({Math.floor(Math.random() * 200) + 1})</span>
-              </div>
-                <br></br>
-                {lineItem ?
-                  <div>
-                    <button onClick={ () => updateLineItem(lineItem.id, { quantity: lineItem.quantity - 1 }) }>-</button>
-                    <span className="price">Quantity: {lineItem.quantity}</span>
-                    <button onClick={ () => updateLineItem(lineItem.id, { quantity: lineItem.quantity + 1 }) }>+</button>
-                    <br></br>
-                    <button onClick={ () => deleteLineItem(lineItem.id) }>Delete</button>
-                  </div>
-                  : null
-                }
-              </div>
-            </div>
-          );
-        })
-        :
-        null
+// const ProductCard = ({ products, lineItems, updateLineItem, deleteLineItem, loggedIn }) => {
+class ProductCard extends Component {
+  constructor({ products, lineItems, updateLineItem, deleteLineItem, loggedIn }) {
+    super();
+    this.state = {
+      lineItems:[]
+    };
+    this.deleteInStorage=this.deleteInStorage.bind(this);
+    this.updateInStorage=this.updateInStorage.bind(this);
+  }
+
+  deleteInStorage (id) {
+    var lineItems = JSON.parse(localStorage.getItem('lineItems'));
+    var newId=0;
+    for (var i =0; i<lineItems.length; i++){
+      if (lineItems[i].productId===id){
+        newId=i;
       }
-    </div>
-  );
-};
+    }
+
+    lineItems.splice(newId,1);
+    store.dispatch({
+      type: 'DELETE_LINEITEM_STORAGE',
+      lineItem: {id}
+    });
+    localStorage.setItem('lineItems', JSON.stringify(lineItems));
+
+  }
+
+  updateInStorage(lineItemNew,id){
+    console.log(lineItemNew)
+    var lineItems = JSON.parse(localStorage.getItem('lineItems'));
+    var newId=0;
+    for (var i =0; i<lineItems.length; i++){
+      if (lineItems[i].productId===id){
+        newId=i;
+      }
+    }
+    lineItems.splice(newId,1);
+    store.dispatch({
+      type: 'DELETE_LINEITEM_STORAGE',
+      lineItem: {id}
+    })
+    lineItems.push(lineItemNew);
+    store.dispatch({
+      type: 'CREATE_LINEITEM_STORAGE',
+      lineItemNew
+    })
+    localStorage.setItem('lineItems', JSON.stringify(lineItems));
+
+  }
+
+  render() {
+    const { products, lineItems, updateLineItem, deleteLineItem, loggedIn } = this.props;
+    
+    return (
+      <div className="wrapper">
+        {
+          products ?
+          products.map(product => {
+            const lineItem = lineItems ? lineItems.find(lineItem => lineItem.productId === product.id) : null;
+            return (
+              <div key={ product.id }>
+                <div className="card card-1">
+                  <span className="card-header">
+                    <img alt="product here" src={ product.imageUrl } />
+                  </span>
+                  <Link to={ `/products/${product.id}` }>
+                    <h3>{product.name}</h3>
+                  </Link>
+                  <span className="price">
+                    Price: ${product.price}
+                  </span>
+                  <div style={{fontSize: 16, display:'flex',justifyContent:'center'}}>
+                    <StarRatingComponent
+                      name="rate1"
+                      editing={false}
+                      starCount={5}
+                      value={4.5}
+                    />
+                    <span>({Math.floor(Math.random() * 200) + 1})</span>
+                </div>
+                  <br></br>
+                  {lineItem && loggedIn?
+                    <div>
+                      <button onClick={ () => updateLineItem(lineItem.id, { quantity: lineItem.quantity - 1 }) }>-</button>
+                      <span className="price">Quantity: {lineItem.quantity}</span>
+                      <button onClick={ () => updateLineItem(lineItem.id, { quantity: lineItem.quantity + 1 }) }>+</button>
+                      <br></br>
+                      <button onClick={ () => deleteLineItem(lineItem.id) }>Delete</button>
+                    </div>
+                    : null
+                  }
+                  {lineItem && !loggedIn?
+                    <div>
+                      <button onClick={ () => this.updateInStorage({ productId: product.id, quantity: lineItem.quantity - 1, price: product.price, name: product.name, active: true }, lineItem.productId) }>-</button>
+                      <span className="price">Quantity: {lineItem.quantity}</span>
+                      <button onClick={ () => this.updateInStorage({ productId: product.id, quantity: lineItem.quantity + 1, price: product.price, name: product.name, active: true }, lineItem.productId) }>+</button>
+                      <br></br>
+                      <button onClick={ () => this.deleteInStorage(lineItem.productId) }>Delete</button>
+                    </div>
+                    : null
+                  }
+                </div>
+              </div>
+            );
+          })
+          :
+          null
+        }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ user }) => {
+  const loggedIn = !!user.id;
+  return {
+    loggedIn,
+    user
+  };
+}
+
 
 const mapDispatchToProps = (dispatch, { history }) => {
   return {
@@ -64,4 +138,4 @@ const mapDispatchToProps = (dispatch, { history }) => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(ProductCard);
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCard);
